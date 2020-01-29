d0dbb234fe313a595267c47cfdb54d979092721b
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-29 19:26:16 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,6 @@
+spice_rack = [
+  ["Mace", "Ginger", "Marojam"],          
+  ["Paprika", "Fajita Mix", "Coriander"], 
+  ["Parsley", "Sage", "Rosemary"]         
+]
+spice_rack[1][100] = "Poodle Dinner"
